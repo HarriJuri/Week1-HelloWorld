15ec03d043c65dc7b716b7eed7fc55b6555cc8eb
HarriJuri <89829639+HarriJuri@users.noreply.github.com>
2021-08-31 12:21:09 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            //Readline() to read input from consol
+            string Username = Console.ReadLine();
+            Console.WriteLine("Hello," + Username);
+            
+            Console.Read();
+        
         }
     }
 }
